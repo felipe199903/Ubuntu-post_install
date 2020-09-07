9ad82bb944c78adc5d9a01e3628c244c14305cc2
Fschorles <schorles21@gmail.com>
2020-09-06 21:38:58 -0300

    create user

--- user.sh
+++ user.sh
@@ -1,5 +1,9 @@
+echo "create user, what is user name?"
+read username
 # create user
-sudo adduser username
+sudo adduser $username
+
+echo "user: " $username "created with success"
 
 # add user to sudo group
-sudo usermod -aG sudo username
+sudo usermod -aG sudo $username
