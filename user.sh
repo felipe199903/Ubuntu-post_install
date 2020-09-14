6459edf8975087ddf5e71995688ceba666fa2540
Felipe <schorles21@gmail.com>
2020-09-14 12:11:11 -0300

    if for sudo

--- user.sh
+++ user.sh
@@ -5,8 +5,14 @@ sudo adduser $username
 
 echo "user: " $username "created with success"
 
-# add user to sudo group
-sudo usermod -aG sudo $username
+echo "add user to SUDO group? (y - yes or n - no)"
+read addRoot
+
+if [ $addRoot == 'y' ]
+then
+    # add user to sudo group
+    sudo usermod -aG sudo $username
+fi
 
 # list users
 cut -d: -f1 /etc/passwd
